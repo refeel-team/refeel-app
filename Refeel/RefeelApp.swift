2b5e82a4e62a536718adb1a182c59118daf2296b
kdn0325 <dn10003@gmail.com>
2025-05-12 11:20:55 +0900

    Initialize Refeel project with essential files and configurations
    - Created project workspace and user interface state files.
    - Added scheme management plist for Xcode.
    - Set up asset catalog with AccentColor and AppIcon.
    - Implemented initial ContentView and RefeelApp structure.

---
+++ Refeel/RefeelApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefeelApp.swift
+//  Refeel
+//
+//  Created by Abel on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct RefeelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
