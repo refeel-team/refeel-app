56cdc9e7915fd96ee7e2beb7d8c461842ede8322
morgan4563 <morganpark4563@gmail.com>
2025-05-12 16:09:46 +0900

    feat: 기본 파일 구조 생성

---
+++ Refeel/Models/Retrospect.swift
@@ -0,0 +1,15 @@
+//
+//  Retrospect.swift
+//  Refeel
+//
+//  Created by hyunMac on 5/12/25.
+//
+
+import Foundation
+
+struct Retrospect: Identifiable {
+    let id: UUID
+    let date: Date
+    let content: String
+    let category: String
+}
