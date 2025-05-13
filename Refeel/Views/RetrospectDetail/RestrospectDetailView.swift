b7737070cf8e76f6b7592d203567920361247b93
kdn0325 <dn10003@gmail.com>
2025-05-13 15:21:53 +0900

    feat: enhance RestrospectDetailView with text editor and additional prompts

--- Refeel/Views/RetrospectDetail/RestrospectDetailView.swift
+++ Refeel/Views/RetrospectDetail/RestrospectDetailView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct RestrospectDetailView: View {
+    @State private var text: String = ""
+
     //유진님
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("카테고리")
+        Text("X")
+        VStack(alignment: .leading) {
+            Text("오늘의 회고")
+                .font(.headline)
+
+            TextEditor(text: $text)
+                .frame(height: 200)
+                .padding()
+                .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray))
+        }
+        .padding()
+        Text("오늘 내가 팀에 기여한 것은 무엇인가요?")
+        Text("어떤 것을 더 하면 내가 더 성장할 수 있을까요?")
+        Text("내일의 todo-list")
+        Button {
+
+        } label: {
+            Text("저장하기")
+        }
+        .buttonStyle(.borderedProminent)
+
     }
 }
 
