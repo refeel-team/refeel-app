c89bc31617b639183fa1fa15e460a37e9f951062
kdn0325 <dn10003@gmail.com>
2025-05-13 16:59:57 +0900

    feat: add FlowLayout view with basic structure

---
+++ Refeel/FlowLayout.swift
@@ -0,0 +1,18 @@
+//
+//  FlowLayout.swift
+//  Refeel
+//
+//  Created by Abel on 5/13/25.
+//
+
+import SwiftUI
+
+struct FlowLayout: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlowLayout()
+}
