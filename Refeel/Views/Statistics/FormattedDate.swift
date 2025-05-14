1ebb2d7cc34a9154ecf85af23e30156e1d95de85
kdn0325 <dn10003@gmail.com>
2025-05-14 16:45:30 +0900

    refactor: remove unused formattedDate function from Statistics view

--- Refeel/Views/Statistics/FormattedDate.swift
+++
@@ -1,5 +0,0 @@
-    private func formattedDate(_ date: Date) -> String {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "YYYY-MM-dd"
-        return formatter.string(from: date)
-    }
