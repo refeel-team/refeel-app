11c571a534f433140496ca8d6b8962b51ebda54e
kdn0325 <dn10003@gmail.com>
2025-05-14 16:45:20 +0900

    refactor: adjust formatting and visibility of formattedDate function

--- Refeel/Util/FormattedDate.swift
+++ Refeel/Util/FormattedDate.swift
@@ -5,9 +5,10 @@
 //  Created by Abel on 5/14/25.
 //
 
+import SwiftUI
 
-    private func formattedDate(_ date: Date) -> String {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "YYYY-MM-dd"
-        return formatter.string(from: date)
-    }
+func formattedDate(_ date: Date) -> String {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "YYYY-MM-dd"
+    return formatter.string(from: date)
+}
