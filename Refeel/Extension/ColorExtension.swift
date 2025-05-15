a85e3c3f7f9f4269cbac817b10279a5218838677
kdn0325 <dn10003@gmail.com>
2025-05-15 10:41:43 +0900

    feat: implement hex color initialization in Color extension

--- Refeel/Extension/ColorExtension.swift
+++ Refeel/Extension/ColorExtension.swift
@@ -5,4 +5,22 @@
 //  Created by Abel on 5/15/25.
 //
 
-import Foundation
+import SwiftUI
+
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        _ = scanner.scanString("#")
+
+        var rgb: UInt64 = 0
+        scanner.scanHexInt64(&rgb)
+
+        let r = Double((rgb >> 16) & 0xFF) / 255.0
+        let g = Double((rgb >>  8) & 0xFF) / 255.0
+        let b = Double((rgb >>  0) & 0xFF) / 255.0
+        self.init(red: r, green: g, blue: b)
+    }
+    static let primaryBlue = Color(hex: "#FF5A28")
+}
