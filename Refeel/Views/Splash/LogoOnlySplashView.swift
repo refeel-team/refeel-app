e38b15984bd843509407c59393fbab6f0678ae01
kdn0325 <dn10003@gmail.com>
2025-05-15 14:46:24 +0900

    feat: implement LogoOnlySplashView with splash image and active state management

--- Refeel/Views/Splash/LogoOnlySplashView.swift
+++ Refeel/Views/Splash/LogoOnlySplashView.swift
@@ -8,11 +8,20 @@
 import SwiftUI
 
 struct LogoOnlySplashView: View {
+    @Binding var isActive: Bool
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.white.ignoresSafeArea()
+            Image("refeel")
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                self.isActive = false
+            }
+        }
     }
 }
 
 #Preview {
-    LogoOnlySplashView()
+    LogoOnlySplashView(isActive: .constant(true))
 }
